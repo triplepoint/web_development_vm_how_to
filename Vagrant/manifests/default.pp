d9578fddcd24a0e5d08752beddc8d04b92d20cf1
Jonathan Hanson <jonathan@jonathan-hanson.org>
2012-12-02 14:02:18 -0800

    attempted to make the puppet manifest more idempotent.  Also added sanity checks for working directory existence.

--- Vagrant/manifests/default.pp
+++ Vagrant/manifests/default.pp
@@ -6,10 +6,16 @@
 #
 ###
 
-$how_to_git_user_name = 'triplepoint'
-$how_to_git_repo_name = 'web_development_vm_how_to'
+$how_to_github_user_name  = 'triplepoint'
+$how_to_github_repo_name  = 'web_development_vm_how_to'
 
-$how_to_git_url       = "git://github.com/${how_to_git_user_name}/${how_to_git_repo_name}.git"
+$how_to_git_url           = "git://github.com/${how_to_github_user_name}/${how_to_github_repo_name}.git"
+
+$which_makefile_target    = 'development_server'
+
+$make_logfile             = "/usr/src/${how_to_github_repo_name}/makelog.log"
+
+
 
 package { 'make':
     ensure    => present
@@ -19,17 +25,26 @@ package { 'git-core':
     ensure    => present
 }
 
+file { '/usr/src':
+    ensure    => directory,
+    mode      => 0644
+}
+
 exec { 'git_clone':
     command   => "/usr/bin/git clone ${how_to_git_url}",
     cwd       => '/usr/src/',
-    creates   => "/usr/src/${how_to_git_repo_name}",
-    require   => Package['git-core']
+    creates   => "/usr/src/${how_to_github_repo_name}",
+    require   => [
+        File['/usr/src'],
+        Package['git-core']
+    ]
 }
 
 exec { 'make_server':
-    command   => 'make development_server 2>&1 | tee makelog.log',
+    command   => "make ${which_makefile_target} 2>&1 | tee ${make_logfile}",
     path      => ['/usr/bin', '/bin', '/usr/local/bin', '/usr/sbin', '/sbin', '/usr/local/sbin', '/opt/vagrant_ruby/bin'],
-    cwd       => "/usr/src/${how_to_git_repo_name}",
+    cwd       => "/usr/src/${how_to_github_repo_name}",
+    creates   => $make_logfile,
     timeout   => 0,
     require   => [
         Exec['git_clone'],
